0624c3b2422f6b75fb9d5b2d17e18beb8d03e980
boomboompower <boomboompowermc@gmail.com>
2017-03-02 17:20:07 +1100

    Initial commit

---
+++ src/main/java/me/boomboompower/alert/simplechatalert/SimpleChatAlert.java
@@ -0,0 +1,17 @@
+package me.boomboompower.alert.simplechatalert;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SimpleChatAlert extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
